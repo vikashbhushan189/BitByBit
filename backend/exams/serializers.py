fb45ea658e0b89e537446b69f50304342e911616
vikash bhushan <vikashbhushan189@gmail.com>
2025-12-12 19:07:03 +0530

    Fix CustomUserSerializer to prevent 500 error on user profile fetch

--- backend/exams/serializers.py
+++ backend/exams/serializers.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
+from djoser.serializers import UserSerializer as BaseUserSerializer
 from .models import Course, Subject, Chapter, Topic, Exam, Question, Option, ExamAttempt, AdBanner
+
+# --- CUSTOM USER SERIALIZER (Fixes 500 Error on /me/) ---
+class CustomUserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_superuser', 'is_staff')
+        ref_name = 'CustomUser' 
+
+# --- APP SERIALIZERS ---
 
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,7 +31,7 @@ class TopicSerializer(serializers.ModelSerializer):
     quiz_id = serializers.SerializerMethodField()
     class Meta:
         model = Topic
-        # Removed study_notes from here
+        # Note: study_notes moved to Chapter, removed from here
         fields = ['id', 'title', 'order', 'quiz_id']
 
     def get_quiz_id(self, obj):
@@ -32,13 +41,14 @@ class ChapterSerializer(serializers.ModelSerializer):
     topics = TopicSerializer(many=True, read_only=True)
     class Meta:
         model = Chapter
-        # Added study_notes here
+        # Note: study_notes is here now
         fields = ['id', 'title', 'order', 'study_notes', 'topics']
 
 class SubjectSerializer(serializers.ModelSerializer):
     chapters = ChapterSerializer(many=True, read_only=True)
     class Meta:
         model = Subject
+        # Includes 'section' for UGC NET grouping
         fields = ['id', 'title', 'section', 'order', 'chapters']
 
 class CourseSerializer(serializers.ModelSerializer):

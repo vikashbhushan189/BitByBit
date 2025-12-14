baa7a2966e5215e37c247ff57ff4e101e4dd15a0
vikash bhushan <vikashbhushan189@gmail.com>
2025-12-14 15:53:42 +0530

    updated nav bar

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { 
     BookOpen, Clock, Trophy, ArrowRight, 
-    BrainCircuit, Zap, Users, LayoutGrid, X
+    BrainCircuit, Zap, Users, LayoutGrid, X, ChevronDown
 } from 'lucide-react';
 import api from '../api/axios';
 

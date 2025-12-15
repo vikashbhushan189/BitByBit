a5028229808b65d8319c787010d78b9716c61176
vikash bhushan <vikashbhushan189@gmail.com>
2025-12-15 18:41:09 +0530

    login using OTP

--- backend/exams/urls.py
+++ backend/exams/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet, ChapterViewSet, ExamViewSet, AuthViewSet, AttemptHistoryViewSet, TopicViewSet,AdBannerViewSet, AIGeneratorViewSet, BulkNotesViewSet
+from django.contrib import admin
+from exams.views import MyTokenObtainPairView
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
@@ -14,4 +16,10 @@ router.register(r'chapters', ChapterViewSet)
 router.register(r'auth-otp', AuthViewSet, basename='auth-otp')
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    # Use Custom View for Password Login
+    path('api/auth/jwt/create/', MyTokenObtainPairView.as_view(), name='jwt-create'),
+    path('api/', include('exams.urls')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),
 ]
